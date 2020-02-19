9100ebca0c4cd700c12f17870066c1951a0bf385
FranPrince <fran.p.21@gmail.com>
2020-02-19 20:22:08 -0300

    crea un monton de promesas

--- src/js/modules/fetchinfo.js
+++ src/js/modules/fetchinfo.js
@@ -1,5 +1,5 @@
 import mostrarPokemonSeleccionado from './ui.js';
-import { agregarCeros, capitalize } from './utils.js';
+import { agregarCeros } from './utils.js';
 
 const urlPokemon = 'https://pokeapi.co/api/v2/pokemon';
 
@@ -12,12 +12,32 @@ async function obtenerDescripcion(idPokemon) {
   }
   return 'No se encontró una descripción en español';
 }
+async function obtenerTipoPokemon(idPokemon) {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${idPokemon}`);
+  const pokemonSpecies = await res.json();
+  const genera = pokemonSpecies.genera.find((categoria) => categoria.language.name === 'es');
+  if (genera) {
+    return genera.genus;
+  }
+  return 'No se encontró una descripción en español';
+}
 
 async function obtenerInfoPokemon(idPokemon) {
   const res = await fetch(`${urlPokemon}/${idPokemon}`);
   const pokemon = await res.json();
   return pokemon;
 }
+async function obtenerHabilidadesEsp(url) {
+  const res = await fetch(url);
+  const abilityNames = await res.json();
+  const nombreEspaniol = abilityNames.names.find((nombres) => nombres.language.name === 'es');
+  if (nombreEspaniol) {
+    return nombreEspaniol.name;
+  } else {
+    return 'No se encontró'
+  }
+
+};
 
 function obtenerFotoPokemon(idPokemon) {
   const urlImagenes = 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/';
@@ -30,35 +50,48 @@ function obtenerInfoDelPokemonSeleccionado(id) {
     id: null,
     nombre: '',
     descripcion: '',
-    habilidades: [],
+    habilidades: {
+      url: [],
+      eng: [],
+      esp: []
+    },
     altura: null,
     peso: null,
     tipos: [],
+    categoria: '',
     foto: '',
   };
-  obtenerDescripcion(id).then((resultadoDescripcion) => {
-    pokemonInfo.descripcion = resultadoDescripcion;
-  })
-  .catch(err => {
-    console.log(err);
-  });
+  console.log(Promise.all);
   pokemonInfo.foto = obtenerFotoPokemon(id);
   obtenerInfoPokemon(id).then((pokemon) => {
     pokemonInfo.id = pokemon.id;
     pokemonInfo.nombre = pokemon.name;
     pokemon.abilities.forEach((habilidad) => {
-      pokemonInfo.habilidades.push(capitalize(habilidad.ability.name));
+      pokemonInfo.habilidades.url.push(habilidad.ability.url);
+      pokemonInfo.habilidades.eng.push(habilidad.ability.name);
     });
     pokemonInfo.altura = pokemon.height * 10;
     pokemonInfo.peso = pokemon.weight / 10;
     pokemon.types.forEach((tipo) => {
       pokemonInfo.tipos.push(tipo.type.name);
     });
-    mostrarPokemonSeleccionado(pokemonInfo)
+    obtenerDescripcion(id).then((resultadoDescripcion) => {
+      pokemonInfo.descripcion = resultadoDescripcion;
+      pokemonInfo.habilidades.url.forEach((habilidad) => {
+        obtenerHabilidadesEsp(habilidad).then((habilidadEsp) => {
+          pokemonInfo.habilidades.esp.push(habilidadEsp);
+        });
+      });
+      obtenerTipoPokemon(id).then((resultadoCategoria) => {
+        pokemonInfo.categoria = resultadoCategoria;
+        mostrarPokemonSeleccionado(pokemonInfo);
+        console.log(pokemonInfo)
+      })
+    })
   })
-  .catch(err => {
-    console.log(err);
-  });
+    .catch(err => {
+      console.log(err);
+    });
 }
 
 export { obtenerInfoDelPokemonSeleccionado, urlPokemon };
--- src/js/modules/ui.js
+++ src/js/modules/ui.js
@@ -2,16 +2,20 @@ import { agregarCeros, capitalize } from './utils.js';
 
 function mostrarPokemonSeleccionado(infoPokemonSeleccionado) {
   const divRowClass = $('<div class="row"></div>');
+  const divInfoRowClass = $('<div class="row indo"></div>');
   const containerPokemon = $('<div id="container-pokemon" class="mt-5 col-md-4"></div>');
   const nombrePokemon = $('<h2 id="nombre-pokemon" class="pt-3 text-capitalize text-center">Cargando...</h2>');
   const containerBadgeTipoPokemon = $('<div id="container-badge-tipo-pokemon" class="container"></div>');
   const containerFotoPokemon = $('<div id="container-foto-pokemon"></div>');
   const fotoPokemon = $('<img id="foto-pokemon" src="./src/img/loading.gif" class="card-img-top" alt="">');
   const containerInfo = $('<div id="container-info" class="col-md-8 mt-5 ml-auto"></div>');
-  const containerInfoTop = $('<div id="container-peso-altura" class="col-md-11 ml-auto mt-0"></div>');
+  const containerInfoTop1 = $('<div id="container-peso-altura" class="col-md-5 ml-auto mt-0"></div>');
+  const containerInfoTop2 = $('<div id="container-peso-altura" class="col-md-5 ml-auto mt-0"></div>');
   const containerInfoBottom = $('<div id="container-descripcion" class="col-md-11 ml-auto mt-0"><p class="titulo">Descripción:</p></div>');
-  const pesoPokemon = $('<div id="pesoPokemon" class="text-left pt-3 col-6">Cargando...</div>');
-  const alturaPokemon = $('<div id="alturaPokemon" class="col-6 text-left">Cargando...</div>');
+  const pesoPokemon = $('<div id="pesoPokemon" class="text-left pt-3">Cargando...</div>');
+  const alturaPokemon = $('<div id="alturaPokemon" class="text-left">Cargando...</div>');
+  const habilidadesPokemon = $('<div id="pesoPokemon" class="text-left">Cargando...</div>');
+  const categoriaPokemon = $('<div id="alturaPokemon" class="text-left pt-3">Cargando...</div>');
 
 
   if ($('#container-pokemon')) {
@@ -33,11 +37,17 @@ function mostrarPokemonSeleccionado(infoPokemonSeleccionado) {
   containerFotoPokemon.append(fotoPokemon);
   fotoPokemon.attr('src', infoPokemonSeleccionado.foto);
   divRowClass.append(containerInfo);
-  containerInfo.append(containerInfoTop);
-  containerInfoTop.append(alturaPokemon);
+  containerInfo.append(divInfoRowClass);
+  divInfoRowClass.append(containerInfoTop1);
+  containerInfoTop1.append(alturaPokemon);
   alturaPokemon.html(`<strong>Altura:</strong> ${infoPokemonSeleccionado.altura} cm`);
-  containerInfoTop.append(pesoPokemon);
+  containerInfoTop1.append(pesoPokemon);
   pesoPokemon.html(`<strong>Peso:</strong> ${infoPokemonSeleccionado.peso} kg`);
+  divInfoRowClass.append(containerInfoTop2);
+  containerInfoTop2.append(habilidadesPokemon);
+  habilidadesPokemon.html(`<strong>Habilidades:</strong> ${infoPokemonSeleccionado.habilidades.esp.join(', ')}`);
+  containerInfoTop2.append(categoriaPokemon);
+  categoriaPokemon.html(`<strong>Categoría:</strong> ${infoPokemonSeleccionado.categoria}`);
   containerInfo.append(containerInfoBottom);
   containerInfoBottom.append(`<p>${infoPokemonSeleccionado.descripcion}</p>`);
 }
